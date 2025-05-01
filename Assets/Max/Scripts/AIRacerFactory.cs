3ad37cc85abb6319672b08c2f6852211a5dc1e2d
Max Schulte <maxgs1001@gmail.com>
2025-05-01 13:19:01 +0200

    Comments

--- Assets/Max/Scripts/AIRacerFactory.cs
+++ Assets/Max/Scripts/AIRacerFactory.cs
@@ -1,22 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class AIRacerFactory : RacerFactory
 {
-
+    // Array of prefabs corresponding to ARacer, BRacer, and CRacer types.
     private GameObject[] prefabs;
+
+    // Pool of suffixes used to make each racer's name unique and identifiable.
     private readonly string[] nameSuffixes =
     {
         "Alpha","Turbo","Viper", "Dash","Omega","Thunder"
     };
 
+    //An array of GameObjects representing racer models
     public AIRacerFactory(GameObject[] prefabs)
     {
         this.prefabs = prefabs;
     }
 
+    //Chooses which racer to create
     public override AIRacer CreateRacer(int type)
     {
         AIRacer racer;
@@ -27,11 +32,15 @@ public class AIRacerFactory : RacerFactory
             case 2: racer = new CRacer(); break;
             default: throw new ArgumentException("Invalid racer type.");
         }
+
         racer.ModelPrefab = prefabs[type];
+
+        // Append a random suffix to the racer's name to ensure uniqueness
         string suffix = GetRandomSuffix();
         racer.RacerName += " " + suffix;
         return racer;
     }
+    //A string suffix for unique naming
     private string GetRandomSuffix()
     {
         return nameSuffixes[UnityEngine.Random.Range(0, nameSuffixes.Length)];
--- Assets/Max/Scripts/RacerSpawner.cs
+++ Assets/Max/Scripts/RacerSpawner.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class RacerSpawner : MonoBehaviour
 {
+    // Array of spawn locations in the scene
     public Transform[] spawnPoints;
 
+    // Prefabs for each type of AI racer
     public GameObject ARacerPrefab;
     public GameObject BRacerPrefab;
     public GameObject CRacerPrefab;
@@ -13,10 +15,16 @@ public class RacerSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Array of prefabs
         GameObject[] prefabTypes = { ARacerPrefab, BRacerPrefab, CRacerPrefab };
-       RacerFactory factory = new AIRacerFactory(prefabTypes);
+
+        // Create the factory and pass in the prefab list
+        RacerFactory factory = new AIRacerFactory(prefabTypes);
+
+
         for (int i = 0; i < spawnPoints.Length; i++)
         {
+            // Cycle through the 3 racer types using modulo
             int type = i % 3;
 
             AIRacer racer = factory.CreateRacer(type);
@@ -24,8 +32,8 @@ public class RacerSpawner : MonoBehaviour
             GameObject instance = Instantiate(racer.ModelPrefab, spawnPoints[i].position, spawnPoints[i].rotation);
             
             instance.name = racer.RacerName;
-            
 
+            // Get the RacingAI component from the spawned object
             RacingAI racingAI = instance.GetComponent<RacingAI>();
             if (racingAI != null)
             {
